4403384225f5193ecde7b759c3cecacc4284e6f9
riteshvish02 <rv504263@gmail.com>
2024-03-26 22:34:55 +0530

    completed basic setup

--- src/config/loggerconfig.js
+++ src/config/loggerconfig.js
@@ -14,3 +14,6 @@ const myFormat = printf(({ level, message, timestamp }) => {
   });
 
   module.exports = logger;
+
+
+  
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const {serverconfig,Logger} = require("./config")
 const apiroutes = require("./routes")
+
 app.use("/api",apiroutes)
 app.listen(serverconfig.PORT,()=>{
     console.log(`server listening on ${serverconfig.PORT}`);
