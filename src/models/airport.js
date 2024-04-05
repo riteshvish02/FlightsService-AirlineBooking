be3178feb6b1a0be1f3c2b9547edebfd9c4da973
riteshvish02 <rv504263@gmail.com>
2024-04-05 13:09:42 +0530

    Created a new migration to associate airport and city model

--- src/models/airport.js
+++ src/models/airport.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.City,{
+        foreignKey: 'cityId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
     }
   }
   Airport.init({
@@ -37,7 +42,6 @@ module.exports = (sequelize, DataTypes) => {
     cityId:{
       type: DataTypes.INTEGER,
       allowNull: false,
-      
     }
   }, {
     sequelize,
